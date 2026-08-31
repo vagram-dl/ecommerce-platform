9d54075db1e20a88cb11cbfdda0aec6643344e71
Katranyan Vagram <vagram.katr@gmail.com>
2026-08-31 18:32:42 +0300

    feat: implement user registration API endpoint with DRF in identity-service

--- services/identity-service/accounts/views.py
+++ services/identity-service/accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import RegisterSerializer
+
+class RegisterView(APIView):
+    permission_classes = []
+
+    def post(self,request):
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+
+            return Response(
+                {
+                    'id': user.id,
+                    'email': user.email,
+                    'message': 'User registered successfully'
+                },
+                status = status.HTTP_201_CREATED
+            )
+
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
