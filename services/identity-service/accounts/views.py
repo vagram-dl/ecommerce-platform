a95143b392f8d9dcd3ab182a81d73dc2f452a49f
Katranyan Vagram <vagram.katr@gmail.com>
2026-09-02 18:39:05 +0300

    feat: implement JWT authentication and login endpoint in identity-service

--- services/identity-service/accounts/views.py
+++ services/identity-service/accounts/views.py
@@ -4,7 +4,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, LoginSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class RegisterView(APIView):
     permission_classes = []
@@ -25,3 +26,23 @@ class RegisterView(APIView):
             )
 
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    permission_classes = []
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data, context={'request':request})
+
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            refresh = RefreshToken.for_user(user)
+
+            return Response(
+                {
+                    'refresh': str(refresh),
+                    'access': str(refresh.access_token),
+                    'message': 'Login successful'
+                },
+                status = status.HTTP_200_OK
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
