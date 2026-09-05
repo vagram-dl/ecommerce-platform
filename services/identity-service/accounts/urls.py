ce5a8bd83fb144b05634f8cf2c9b63d872702591
Katranyan Vagram <vagram.katr@gmail.com>
2026-09-05 22:22:21 +0300

    feat: implement logout endpoint with JWT token blacklisting

--- services/identity-service/accounts/urls.py
+++ services/identity-service/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import RegisterView, UserView
-from .views import RegisterView,LoginView
+from .views import LoginView
+from .views import LogoutView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(),name='login'),
-    path('me/', UserView.as_view(), name='me')
+    path('me/', UserView.as_view(), name='me'),
+    path('logout/', LogoutView.as_view(),name='logout'),
 ]
--- services/identity-service/accounts/views.py
+++ services/identity-service/accounts/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer, LogoutSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated
 
@@ -54,3 +54,14 @@ class UserView(APIView):
     def get(self,request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self,request):
+        serializer = LogoutSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
